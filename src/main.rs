0ce25ba678c0f5260aa6052c409a28a6e9cf0dac
BraydenMarshall <bmarsh579@gmail.com>
2019-06-22 18:56:14 -0700

    Evaluator is fixed, commands now need to be re-added

--- src/main.rs
+++ src/main.rs
@@ -33,179 +33,134 @@ fn main() {
         )
         .get_matches();
 
-    let mut t = Turtle::new();
-    let debug: bool = matches.is_present("debug");
-    let mut vars: HashMap<String, Expression> = HashMap::new();
-    let mut procedures: HashMap<String, AST> = HashMap::new();
-    let turtle_commands = get_turtle_commands();
+    let mut evaluator = Evaluator {
+        turtle: Turtle::new(),
+        vars: HashMap::new(),
+        procedures: HashMap::new(),
+        commands: get_turtle_commands(),
+        debug: matches.is_present("debug"),
+    };
 
     // if a script argument was passed, run the script
     if let Some(file) = matches.value_of("SCRIPT") {
-        run_program(&mut t, &fs::read_to_string(file).unwrap(), debug, &mut vars, &mut procedures, &turtle_commands);
+        evaluator.run_program(&fs::read_to_string(file).unwrap());
     }
 
     // running interactive shell
     loop {
         let input = get_input();
 
-        run_program(&mut t, &input, debug, &mut vars, &mut procedures, &turtle_commands);
+        evaluator.run_program(&input);
     }
 }
 
-fn run_program(
-    t: &mut Turtle,
-    input: &str,
+pub struct Evaluator {
+    turtle: Turtle,
+    vars: HashMap<String, Expression>,
+    procedures: HashMap<String, AST>,
+    commands: HashMap<String, TurtleCommand>,
     debug: bool,
-    vars: &mut HashMap<String, Expression>,
-    procedures: &mut HashMap<String, AST>,
-    turtle_commands: &HashMap<String, TurtleCommand>
-) {
-    // lexing input and returning vector of tokens
-    let mut lexer = Lexer::new(&input);
-    let mut tokens: Vec<Token> = Vec::new();
-    while let Some(lex_result) = lexer.next() {
-        match lex_result {
-            Ok(tok) => tokens.push(tok),
-            Err(e) => {
+}
+
+impl Evaluator {
+    pub fn run_program(
+        &mut self,
+        input: &str,
+    ) {
+        // lexing input and returning vector of tokens
+        let mut lexer = Lexer::new(&input);
+        let mut tokens: Vec<Token> = Vec::new();
+        while let Some(lex_result) = lexer.next() {
+            match lex_result {
+                Ok(tok) => tokens.push(tok),
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    return;
+                }
+            }
+        }
+
+        if self.debug {
+            println!("Lexing phase completed without error");
+            println!("{:?}", tokens);
+        }
+
+        let mut parser = Parser::new(&tokens);
+
+        // building the AST out of the tokens and running the program
+        // based off of the AST
+        match parser.build_ast() {
+            Ok(ast) => {
+                if self.debug {
+                    println!("Parsing phase completed without error");
+                    println!("{:?}", ast);
+                }
+                self.run_ast(&ast);
+            }
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
+
+    fn run_ast(
+        &mut self,
+        ast: &AST,
+    ) {
+        for stmt in ast.statements.iter() {
+            if let Err(e) = self.run_statement(stmt) {
                 eprintln!("{:?}", e);
-                return;
             }
         }
     }
 
-    if debug {
-        println!("Lexing phase completed without error");
-        println!("{:?}", tokens);
-    }
-
-    let mut parser = Parser::new(&tokens);
-
-    // building the AST out of the tokens and running the program
-    // based off of the AST
-    match parser.build_ast() {
-        Ok(ast) => {
-            if debug {
-                println!("Parsing phase completed without error");
-                println!("{:?}", ast);
-            }
-            run_ast(t, &ast, vars, procedures, turtle_commands);
-        }
-        Err(e) => eprintln!("{:?}", e),
-    }
-}
-
-fn run_ast(
-    t: &mut Turtle,
-    ast: &AST,
-    vars: &mut HashMap<String, Expression>,
-    procedures: &mut HashMap<String, AST>,
-    turtle_commands: &HashMap<String, TurtleCommand>
-) {
-    for stmt in ast.statements.iter() {
-        if let Err(e) = run_statement(t, stmt, vars, procedures, turtle_commands) {
-            eprintln!("{:?}", e);
-        }
-    }
-}
-
-fn run_statement(
-    t: &mut Turtle,
-    stmt: &Statement,
-    vars: &mut HashMap<String, Expression>,
-    procedures: &mut HashMap<String, AST>,
-    turtle_commands: &HashMap<String, TurtleCommand>
-) -> Result<(), String> {
-    // currently does not handle varying argument types,
-    // only accept LOGO number values as command arguments
-    match stmt {
-        Statement::ProcedureDeclaration { name, body } => {
-            if let Some(_) = procedures.get(name) {
-                return Err(format!("Procedure with name {} already exists.", name));
-            }
-
-            procedures.insert(name.to_string(), body.clone());
-        }
-
-        Statement::ProcedureCall { name, args } => {
-            if let Some(func) = turtle_commands.get(name) {
-                let mut _args: Vec<isize> = Vec::new();
-                for i in 0..args.len() {
-                    _args.push(evaluate_expression(&args[i], vars)?);
+    fn run_statement(
+        &mut self,
+        stmt: &Statement,
+    ) -> Result<(), String> {
+        // currently does not handle varying argument types,
+        // only accept LOGO number values as command arguments
+        match stmt {
+            Statement::ProcedureDeclaration { name, body } => {
+                if let Some(_) = self.procedures.get(name) {
+                    return Err(format!("Procedure with name {} already exists.", name));
                 }
-                (*func)(t, &_args); 
-            } else {
-                let ast = match procedures.get(name) {
-                    Some(ast) => ast.clone(),
-                    None => return Err("Undeclared procedure name.".to_string()),
+
+                self.procedures.insert(name.to_string(), body.clone());
+            }
+
+            Statement::ProcedureCall { name, args } => {
+                if let Some(func) = self.commands.get(name) {
+                    let mut _args: Vec<isize> = Vec::new();
+                    for i in 0..args.len() {
+                        _args.push(evaluate_expression(&args[i], &mut self.vars)?);
+                    }
+                    (*func)(&mut self.turtle, &_args); 
+                } else {
+                    let ast = match self.procedures.get(name) {
+                        Some(ast) => ast.clone(),
+                        None => return Err("Undeclared procedure name.".to_string()),
+                    };
+                    self.run_ast(&ast);
+                }
+            },
+
+            Statement::VariableDeclaration { name, val } => {
+                let _val = (**val).clone();
+                let expr = Expression::Number {
+                    val: evaluate_expression(&_val, &mut self.vars)?,
                 };
-                run_ast(t, &ast, vars, procedures, turtle_commands);
-            }
-        },
 
-        Statement::VariableDeclaration { name, val } => {
-            let _val = (**val).clone();
-            let expr = Expression::Number {
-                val: evaluate_expression(&_val, vars)?,
-            };
-
-            vars.insert(name.to_string(), expr);
-        }
-
-        /*
-        Statement::Command {
-            command,
-            args: _args,
-        } => (),
-            let mut args: Vec<f64> = Vec::new();
-            for arg in _args.iter() {
-                args.push(evaluate_expression(arg, vars)? as f64);
+                self.vars.insert(name.to_string(), expr);
             }
 
-            match command {
-                // 0 arity
-                Command::PenUp => t.pen_up(),
-                Command::PenDown => t.pen_down(),
-                Command::HideTurtle => t.hide(),
-                Command::ShowTurtle => t.show(),
-                Command::Home => t.home(),
-                Command::ClearScreen => {
-                    t.clear();
-                    t.home()
+            Statement::Repeat { count, body } => {
+                for _ in 0..*count {
+                    self.run_ast(body);
                 }
-                Command::Clean => t.clear(),
-                Command::Fill => return Err(String::from("Fill not yet implemented")),
-                Command::Exit => std::process::exit(0),
-
-                // 1 arity
-                Command::Forward => t.forward(args[0]),
-                Command::Backward => t.backward(args[0]),
-                Command::Left => t.left(args[0]),
-                Command::Right => t.right(args[0]),
-                Command::SetPenSize => t.set_pen_size(args[0]),
-                Command::SetHeading => t.set_heading(args[0]),
-                Command::Show => println!("{}", args[0]),
-
-                // 2 arity
-                Command::SetXY => t.go_to([args[0], args[1]]),
-
-                // 3 arity
-                Command::SetPenColor => t.set_pen_color([args[0], args[1], args[2]]),
-                Command::SetFillColor => t.set_fill_color([args[0], args[1], args[2]]),
-                Command::SetScreenColor => t
-                    .drawing_mut()
-                    .set_background_color([args[0], args[1], args[2]]),
             }
         }
-        */
 
-        Statement::Repeat { count, body } => {
-            for _ in 0..*count {
-                run_ast(t, body, vars, procedures, turtle_commands);
-            }
-        }
+        Ok(())
     }
-
-    Ok(())
 }
 
 fn evaluate_expression(
@@ -221,7 +176,8 @@ fn evaluate_expression(
             },
             None => Err(format!("Variable {} does not exist", name)),
         },
-        Expression::ArithmeticExpression { postfix } => Ok(evaluate_postfix(postfix, vars)?),
+        Expression::ArithmeticExpression { postfix } =>
+            Ok(evaluate_postfix(postfix, vars)?),
         _ => Err(String::from("There was an errorrrror")),
     }
 }
