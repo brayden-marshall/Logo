bd9f11c20174d29d7056d1c78e404412d666af1f
BraydenMarshall <bmarsh579@gmail.com>
2019-10-26 11:52:11 -0700

    Started adding command type for refactoring evaluator.rs

--- src/commands.rs
+++ src/commands.rs
@@ -16,6 +16,60 @@ where
     }
 }
 
+pub enum Command {
+    // movement
+    Forward,
+    Backward,
+    Left,
+    Right,
+    SetHeading,
+    SetXY,
+    Home,
+
+    // pen
+    PenUp,
+    PenDown,
+    SetPenSize,
+    SetPenColor,
+
+    // other
+    HideTurtle,
+    ShowTurtle,
+    ClearScreen,
+    Clean,
+    SetScreenColor,
+    Show,
+    Exit,
+}
+
+impl Command {
+    pub fn from_str(s: &str) -> Option<Self> {
+        use Command::*;
+        let command = match s {
+            "forward" | "fd" => Forward,
+            "backward" | "bk" => Backward,
+            "left" | "lt" => Left,
+            "right" | "rt" => Right,
+            "setheading" | "seth" => SetHeading,
+            "setxy" => SetXY,
+            "home" => Home,
+            "penup" | "pu" => PenUp,
+            "pendown" | "pd" => PenDown,
+            "setpensize" => SetPenSize,
+            "setpencolor" | "setpc" => SetPenColor,
+            "hideturtle" | "ht" => HideTurtle,
+            "showturtle" | "st" => ShowTurtle,
+            "clearscreen" | "cs" => ClearScreen,
+            "clean" => Clean,
+            "setscreencolor" | "setsc" => SetScreenColor,
+            "show" => Show,
+            "exit" => Exit,
+            _ => return None,
+        };
+        Some(command)
+    }
+}
+
 pub fn get_turtle_commands() -> HashMap<String, TurtleCommand> {
     vec![
         // movement
